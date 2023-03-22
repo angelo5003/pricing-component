ec5edb5a4500bfe65a6db22d53985d4f1ccf6369
Tony5300 <tony_tolen@ziggo.nl>
2023-03-22 18:04:36 +0100

    feat: created the card component and dynamically create new cards with new data.

---
+++ src/components/PricingCard/PricingCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./PricingCardStyle.css";
+
+const PricingCard = ({ card }) => {
+  return (
+    <article className="card_outer_container">
+      <div className="card_inner_container">
+        <p className="card_title">{card.title}</p>
+        <h1 className="card_price">&#36; {card.monthly}</h1>
+        <ul className="card_list_container">
+          <li className="card_list_item">{card.storage}</li>
+          <li className="card_list_item">{card.users}</li>
+          <li className="card_list_item">{card.upload}</li>
+        </ul>
+        <button>{card.learn}</button>
+      </div>
+    </article>
+  );
+};
+
+export default PricingCard;
