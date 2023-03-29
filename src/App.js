7e5753b61c4bab1485d009b33e6fc1334b859e68
Tony5300 <tony_tolen@ziggo.nl>
2023-03-29 17:34:03 +0200

    style: finished all the styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import PricingCard from "./components/PricingCard/PricingCard";
 import data from "./data/data.json";
 import "./App.css";
 import Switch from "./components/Switch/Switch";
+import bottomImg from "./assets/images/bg-bottom.svg";
+import topImg from "./assets/images/bg-top.svg";
 
 function App() {
   const [toggled, setToggled] = useState(false);
 
   return (
     <div>
+      <div className="top_img_container">
+        <img src={topImg} alt="top" className="background_img" />
+      </div>
       <div className="app_container">
         <header className="switch_header">
           <h1>Our Pricing</h1>
@@ -22,6 +27,9 @@ function App() {
               );
             })}
         </div>
+      </div>
+      <div className="bottom_img_container">
+        <img src={bottomImg} alt="bottom" className="background_img" />
       </div>
     </div>
   );
